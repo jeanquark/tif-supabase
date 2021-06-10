84ceae29d9c4e2096d72f0a1dc613f9356810b5b
unknown <jm.kleger@gmail.com>
2021-06-10 18:33:10 +0200

    Fetch live score

--- pages/api/api-football/fetch-live-fixtures.js
+++ pages/api/api-football/fetch-live-fixtures.js
@@ -5,7 +5,7 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.
 export default async function fetchNextFixtures(req, res) {
     try {
         // 1) Request data from Football API
-        const data = await fetch("https://v3.football.api-sports.io/fixtures?live=all", {
+        const data = await fetch("https://v3.football.api-sports.io/fixtures?league=652&live=all", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": process.env.API_FOOTBALL_KEY,
@@ -17,6 +17,20 @@ export default async function fetchNextFixtures(req, res) {
 
         console.log('response.length: ', response.length)
 
+        // 2) Update DB
+        let array = []
+        for (let i = 0; i < response.length; i++) {
+            array.push({
+                fixture_id: response[i]['fixture']['id'],
+                home_team_score: response[i]['goals']['home'],
+                visitor_team_score: response[i]['goals']['away']
+            })
+        }
+        console.log('array: ', array)
+        // const { data, error } = await supabase
+        //     .from('events').upsert(array, { onConflict: 'fixture_id'})
+
+        return res.status(200).json({ success: true, array })
         return res.status(200).json({ success: true, length: response.length, data: response });
     } catch (error) {
         return res.status(500).json('An error occured on the server: ', error);
