0d79c1302cb64750ef151040ad8cfc5e861e9496
Jean-Marc <jm.kleger@gmail.com>
2021-06-11 01:42:53 +0200

    Corrected bug in api request

--- pages/api/api-football/fetch-live-fixtures.js
+++ pages/api/api-football/fetch-live-fixtures.js
@@ -5,13 +5,13 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.
 export default async function fetchNextFixtures(req, res) {
     try {
         // 1) Request data from Football API
-        const data = await fetch("https://v3.football.api-sports.io/fixtures?league=652&live=all", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": process.env.API_FOOTBALL_KEY,
-                "x-rapidapi-host": "v3.football.api-sports.io",
-                "useQueryString": true
-            }
+        const data = await fetch('https://v3.football.api-sports.io/fixtures?league=652&live=all', {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': process.env.API_FOOTBALL_KEY,
+                'x-rapidapi-host': 'v3.football.api-sports.io',
+                useQueryString: true,
+            },
         })
         const { response } = await data.json()
 
@@ -23,18 +23,16 @@ export default async function fetchNextFixtures(req, res) {
             array.push({
                 fixture_id: response[i]['fixture']['id'],
                 home_team_score: response[i]['goals']['home'],
-                visitor_team_score: response[i]['goals']['away']
+                visitor_team_score: response[i]['goals']['away'],
             })
         }
         // console.log('array: ', array)
-        const { data, error } = await supabase
-            .from('events').upsert(array, { onConflict: 'fixture_id'})
+        const { error } = await supabase.from('events').upsert(array, { onConflict: 'fixture_id' })
+        console.log('error: ', error)
 
         return res.status(200).json({ success: true, array })
         // return res.status(200).json({ success: true, length: response.length, data: response });
     } catch (error) {
-        return res.status(500).json('An error occured on the server: ', error);
-
+        return res.status(500).json('An error occured on the server: ', error)
     }
-
 }
